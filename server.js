97df199fcaf2d64ca9b4cb141673fad9c42f33c4
HoangVu <vuhoangtruong1998@gmail.com>
2023-02-20 19:57:38 +0700

    FEAT:Add new project source

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require("./src/app");
+
+PORT = 8009
+
+const server = app.listen(PORT, ()=>{
+    console.log(`Server start with port: ${PORT}`);
+});
+
+process.on("SIGINT", ()=>{
+    server.close(()=> console.log("Exit Server"))
+});
